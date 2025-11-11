943c171cfc7452e07972404346b2ea7ff5b639bb
thewinemascotke <thewinemascotke@gmail.com>
2025-11-11 19:26:51 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
-import '@styles/globals.css'
+import '../styles/globals.css'
+import { useEffect } from 'react';
 
-function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+  
+<script src="https://sf-cdn.coze.com/obj/unpkg-va/flow-platform/chat-app-sdk/1.2.0-beta.6/libs/oversea/index.js"></script>
+<script>
+  new CozeWebSDK.WebChatClient({
+    config: {
+      bot_id: '7568451471281618945',
+    },
+    componentProps: {
+      title: 'Coze',
+    },
+    auth: {
+      type: 'token',
+      token: 'pat_********',
+      onRefreshToken: function () {
+        return 'pat_********'
+      }
+    }
+  });
+</script>
 
-export default Application
+   
+
+export default MyApp
