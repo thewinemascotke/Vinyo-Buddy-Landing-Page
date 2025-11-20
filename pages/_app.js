d7ab39d68ec5fbf7ec1d4b862f8717f23bf9dc6c
thewinemascotke <thewinemascotke@gmail.com>
2025-11-20 22:37:26 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,15 +4,16 @@ import Script from 'next/script'
 function Application({ Component, pageProps }) {
   return (
     <>
+      {/* This renders your beautiful website pages */}
       <Component {...pageProps} />
       
-      {/* Botpress Main Library */}
+      {/* 1. Botpress Main Library */}
       <Script 
         src="https://cdn.botpress.cloud/webchat/v3.3/inject.js" 
         strategy="afterInteractive"
       />
       
-      {/* Botpress Configuration (Your Unique Bot) */}
+      {/* 2. Botpress Configuration (Vinyo Buddy) */}
       <Script 
         src="https://files.bpcontent.cloud/2025/11/20/18/20251120184757-GWRR1APM.js" 
         strategy="afterInteractive" 
@@ -22,5 +23,3 @@ function Application({ Component, pageProps }) {
 }
 
 export default Application
-
-
