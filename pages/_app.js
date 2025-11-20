2c7ca3cb86e98630b12502234e6226b1a6762808
thewinemascotke <thewinemascotke@gmail.com>
2025-11-20 23:06:18 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,18 @@
-import '../styles/globals.css'  // <--- FIXED: Changed '@' back to '..'
+import '@styles/globals.css' 
 import Script from 'next/script'
 
 function Application({ Component, pageProps }) {
   return (
     <>
-      {/* This renders your website content */}
       <Component {...pageProps} />
       
-      {/* Botpress Script 1: The Injector */}
+      {/* Botpress Script 1 */}
       <Script 
         src="https://cdn.botpress.cloud/webchat/v3.3/inject.js" 
         strategy="lazyOnload" 
       />
       
-      {/* Botpress Script 2: Your Vinyo Buddy Config */}
+      {/* Botpress Script 2 */}
       <Script 
         src="https://files.bpcontent.cloud/2025/11/20/18/20251120184757-GWRR1APM.js" 
         strategy="lazyOnload" 
